184341d4ec1ac6e1f82954be57dca815bd0446df
p4n1k0 <gabre.tarick@gmail.com>
2023-04-05 17:13:00 -0300

    funcao q tras infos sobre arqv criado

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,19 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for item in range(len(instance)):
+        if instance.search(item)['nome_do_arquivo'] == path_file:
+           None
+    file = txt_importer(path_file)
+    if file:
+        data = {
+            'nome_do_arquivo': path_file,
+            'qtd_linhas': len(file),
+            'linhas_do_arquivo': file
+        }
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
