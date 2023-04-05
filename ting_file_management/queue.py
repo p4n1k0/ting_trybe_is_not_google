14e37a706d324822376dfee6a1898fe829fc76ca
p4n1k0 <gabre.tarick@gmail.com>
2023-04-05 14:43:01 -0300

    remove elemento a mais tempo na fila, modificando seu tamanho

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,7 +3,7 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        self.queue = list()
+        self.queue = []
 
     def __len__(self):
         return len(self.queue)
@@ -12,7 +12,9 @@ class Queue(AbstractQueue):
         self.queue.append(value)
 
     def dequeue(self):
-        """Aqui irá sua implementação"""
+        if len(self.queue) != 0:
+            return self.queue.pop(0)
+        None
 
     def search(self, index):
         """Aqui irá sua implementação"""
