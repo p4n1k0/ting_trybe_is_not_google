563fd620f4ac0ded7e6f904e990046641a01a1c8
p4n1k0 <gabre.tarick@gmail.com>
2023-04-05 16:32:13 -0300

    fix identa

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -3,9 +3,9 @@ import sys
 
 def txt_importer(path_file):
     if not path_file.endswith('.txt'):
-       return sys.stderr.write("Formato inválido")
+        return sys.stderr.write("Formato inválido")
     try:
-       with open(path_file) as file:
-           return file.read().split('\n')
+        with open(path_file) as file:
+            return file.read().split('\n')
     except FileNotFoundError:
-       return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
+        return sys.stderr.write(f"Arquivo {path_file} não encontrado\n")
