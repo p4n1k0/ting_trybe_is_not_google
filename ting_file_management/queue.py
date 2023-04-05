45b131139d8bdb3cffd0e7cc01602eb35353724b
p4n1k0 <gabre.tarick@gmail.com>
2023-04-04 21:08:58 -0300

    metodo enqueue add elemento a fila, modificando seu tamanho

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,13 +3,13 @@ from ting_file_management.abstract_queue import AbstractQueue
 
 class Queue(AbstractQueue):
     def __init__(self):
-        """Inicialize sua estrutura aqui"""
+        self.queue = list()
 
     def __len__(self):
-        """Aqui irá sua implementação"""
+        return len(self.queue)
 
     def enqueue(self, value):
-        """Aqui irá sua implementação"""
+        self.queue.append(value)
 
     def dequeue(self):
         """Aqui irá sua implementação"""
