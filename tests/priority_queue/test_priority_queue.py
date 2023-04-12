f1581e07ca8058b8d42f4abfc05427a1b9c66ff5
p4n1k0 <gabre.tarick@gmail.com>
2023-04-12 18:47:09 -0300

    identa

--- tests/priority_queue/test_priority_queue.py
+++ tests/priority_queue/test_priority_queue.py
@@ -4,7 +4,7 @@ import pytest
 
 def test_basic_priority_queueing():
     mock = [{'qtd_linhas': 7}, {'qtd_linhas': 6}, {'qtd_linhas': 1},
-        {'qtd_linhas': 2}, {'qtd_linhas': 3}, ]
+            {'qtd_linhas': 2}, {'qtd_linhas': 3}, ]
 
     p_queue = PriorityQueue()
 
